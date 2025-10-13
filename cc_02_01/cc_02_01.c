97a34c9a4851ab704e9df22c690468ddad0177ef
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2025-10-13 23:57:15 +0530

    feat: chapter 2, program 1

---
+++ cc_02_01/cc_02_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()	{
+	/* I am a comment */
+	printf("Hello world\n");
+	printf("Answer %d\n", 42);
+	printf("Name %s\n", "Sarah");
+	printf("x %.1f i %d\n", 3.5, 100);
+
+	return 0;
+}
